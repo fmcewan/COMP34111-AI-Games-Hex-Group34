3358c22037c548b2f80fd978a51606a3c0a1bbb8
Josh Lennon <joshua.lennon@student.manchester.ac.uk>
2024-11-28 15:43:27 +0000

    Started to implement Node Header file

---
+++ agents/Group34/MCTS/Node.h
@@ -0,0 +1,20 @@
+// Imports
+
+#include <vector>
+#include <memory>
+
+
+class Node {
+
+    private:
+
+        int visits; // times node has been visited
+        float accReward; // award accumulated by node
+        bool end; // true if node is terminal
+
+    public:
+
+
+        
+
+};
